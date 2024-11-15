634cfbfdbdb1a8934a071fd89ef8dd28834d0e3e
axel delliere <axel.delliere@free.fr>
2024-11-15 23:37:46 +0100

    V3

--- app/app.py
+++ app/app.py
@@ -7,130 +7,88 @@ st.markdown("""# Are you eligible to borrow?
 
 #->> Personal informations <<-
 st.markdown("""### About you : """)
+columns = st.columns(3)
 
 #age
-age_input = int(st.slider('Age', 18, 99, 25))
+age_input = int(columns[0].slider('Age', 18, 99, 25))
 
-#Gender
-gender_input= st.radio('Gender', ('Sex Not Available', 'Female', 'Male', 'Joint'))
+if age_input<25:
+    age_input="<25"
+elif age_input<35:
+    age_input="25-34"
+elif age_input<45:
+    age_input="35-44"
+elif age_input<55:
+    age_input="45-54"
+elif age_input<65:
+    age_input="55-64"
+elif age_input<75:
+    age_input="65-74"
+else:
+    age_input=">74"
 
 #incomes
-income_input = int(st.number_input('Income'))
+income_input = columns[1].number_input('Annual income', min_value=0, step=5000, format='%d')
 
-#open credit
-if st.checkbox('Any open credit?'):
-    st.text('Yes')
-    open_credit_input='opc'
-else:
-    st.text('No')
+open_credit_input= columns[2].radio('Have you ever subscribed to a loan?', ('First time', 'I plead guilty !'))
+if open_credit_input=='Personal loan':
     open_credit_input='nopc'
-
-#region
-region_input= st.radio('Region', ('south', 'North', 'central',  'North-East'))
+else:
+    open_credit_input='opc'
 
 #->> Project informations <<-
 st.markdown("""### About your project : """)
+columns = st.columns(2)
 
 #business_or_commercial
-if st.checkbox('Business credit?'):
-    st.text('Business or commercial loan')
-    business_or_commercial_input='ob/c'
-else:
-    st.text('Personal loan')
+business_or_commercial_input= columns[0].radio('Is it for business or personal use?', ('Personal loan', 'Business or commercial loan'))
+if business_or_commercial_input=='Personal loan':
     business_or_commercial_input='nob/c'
-
-#construction_type
-construction_type_input= st.radio('Construction type', ('Site built', 'Land to construct'))
-if construction_type_input == 'Site built':
-    construction_type_input ='sb'
-    Secured_by_input='home'
-    Security_Type_input='direct'
 else:
-    construction_type_input ='mh'
-    Secured_by_input='land'
-    Security_Type_input='Indirect'
-
-
-#credit type
-    # /!\ stand  by
-
-#interest only
-interest_only_input= st.radio('Monthly payment', ('In fine loan', 'Amortizing Loan'))
-if interest_only_input=="In fine loan":
-    st.text('You pay only interest every month and the principal is repaid at the end')
-    interest_only_input='int_only'
-else:
-    st.text('You repaid monthly the principal with interest')
-    interest_only_input='not_int'
-
-
-#loan_amount
-loan_amount_input = int(st.number_input('How much do you need'))
-
-#lump_sum_payment
-if st.checkbox('Ok with lump sum concept ?'):
-    st.text('''Let's go, money is not a problem at this stage''')
-    lump_sum_payment_input='lpsm'
-else:
-    st.text('No way, not a cent more')
-    lump_sum_payment_input='not_lpsm'
-
-#occupancy_type
-occupancy_type_input= st.radio('Occupancy type', ('primary residence', 'secondary residence','investment property'))
-if occupancy_type_input == 'primary residence':
-    occupancy_type_input = "pr"
-elif occupancy_type_input == 'secondary residence':
-   occupancy_type_input="sr"
-elif occupancy_type_input =="investment property":
-    occupancy_type_input="ir"
-
+    business_or_commercial_input='ob/c'
 
 #property value
-property_value_input = int(st.number_input('Property value'))
+property_value_input = int(columns[1].number_input('Property value', min_value=10000, step=5000, format='%d'))
+
+#->> Project informations <<-
+st.markdown("""### About the loan : """)
+columns = st.columns(2)
+
+#loan_amount
+loan_amount_input = int(columns[0].number_input('How much do you need?', min_value=10000, step=5000, format='%d'))
 
 #terms
-term_input = int(st.slider('Term ?', 5, 30, 20) *12)
-st.text(f'equal to {term_input} months')
-
-#total_units
-total_units_input = st.slider('Number of property being financed', 1, 4, 1)
-total_units_input =f'{total_units_input}U'
-
+term_input = int(columns[1].slider('How long do you want to sign up for? (years)', 5, 30, 20) *12)
 
 #API part
     #request construction
-API_URL="https://my-api-app-automated-loan-review-project-qzju5w56rq-ew.a.run.app"
+API_URL="https://automated-loan-review-project-qzju5w56rq-ew.a.run.app" #"https://my-api-app-automated-loan-review-project-qzju5w56rq-ew.a.run.app"
 url = f"{API_URL}/predict"
 
 params = {
     'age': age_input,
-    #'business_or_commercial' :business_or_commercial_input,
-    #'construction_type': construction_type_input,
-    #'credit_type' : ??
-    #'Gender' : gender_input,
+    'business_or_commercial' :business_or_commercial_input,
     'income' : income_input,
-    #'interest_only' : interest_only_input,
-    'loan_limit' : loan_amount_input,
-    #'lump_sum_payment':lump_sum_payment_input,
-    #'occupancy_type':occupancy_type_input,
-    #'open_credit' : open_credit_input,
-    #'property_value' : property_value_input,
-    #'Region' : region_input,
-    #'Secured_by' : Secured_by_input,
-    #'Security_Type' : Security_Type_input,
-    #'term' : term_input,
-    #'total_units' : total_units_input,
+    'loan_amount' : loan_amount_input,
+    #'loan_limit' : loan_amount_input,
+    'open_credit' : open_credit_input,
+    'property_value' : property_value_input,
+    'term' : term_input,
 }
 
 img_url_no="https://ih1.redbubble.net/image.1736163822.8931/bg,f8f8f8-flat,750x,075,f-pad,750x1000,f8f8f8.jpg"
 img_url_yes="https://ih1.redbubble.net/image.1732971092.0283/bg,f8f8f8-flat,750x,075,f-pad,750x1000,f8f8f8.jpg"
 
-response = requests.get(url, params=params).json()
-if st.button("Best we can do"):
-    st.markdown(f"**{str(response['prediction'])}**")
 
-    if response['prediction'][-15:]=="is not approved":
-        st.image(img_url_no,  width=200)
+if st.button("Click to discover the best we can do"):
+
+    response = requests.get(url, params=params).json()
+    columns = st.columns(2)
+    columns[1].markdown(f"**{str(response['status'])}**")
+
+    if response['status'][-15:]=="is not approved":
+        columns[0].image(img_url_no,  width=200)
 
     else:
-        st.image(img_url_yes,  width=200)
+        columns[1].markdown(f"at {response['interest_rate']} !!")
+        columns[0].image(img_url_yes,  width=200)
